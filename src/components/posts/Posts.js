3c113bbea892abd30c192c50f5881adf009cbabc
Romaric Zounlome <rzounlom@gmail.com>
2024-05-02 23:07:19 -0400

    complete posts page; add post service api

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,7 +1,42 @@
 import "./Posts.css";
 
+import { useEffect, useState } from "react";
+
+import PostList from "./PostList";
+import { Spinner } from "react-bootstrap";
+// import { defaultPosts } from "../../data/posts";
+import { getPosts } from "../../services/postsService";
+
 const Posts = () => {
-  return <div className="posts">Posts</div>;
+  const [posts, setPosts] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const fetchPosts = async () => {
+    try {
+      setLoading(true);
+      const posts = await getPosts();
+      // console.log(posts);
+      setPosts(posts);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="posts">
+      {loading ? (
+        <Spinner animation="border" variant="primary" />
+      ) : (
+        <PostList posts={posts} />
+      )}
+    </div>
+  );
 };
 
 export default Posts;
